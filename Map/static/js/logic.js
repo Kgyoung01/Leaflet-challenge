43d056b4c538d6a3635b1f3d78d68967508916ef
Kgyoung01 <102869471+Kgyoung01@users.noreply.github.com>
2022-08-28 23:19:29 -0400

    Add files via upload

---
+++ Map/static/js/logic.js
@@ -0,0 +1,56 @@
+// Creating the map object
+var myMap = L.map("map", {
+  center: [41.257160, -95.995102],
+  zoom: 4 
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Use this link to get the GeoJSON data.
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Getting our GeoJSON data
+d3.json(link).then(function(data) {
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into a function
+  // to calculate the radius.
+  function styleInfo(feature) {
+    return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(),
+        stroke: true,
+        weight: 0.5
+  };
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+      if (magnitude === 0) {
+      return 1;
+      }
+      return magnitude * 4;
+      }
+  }
+
+// Creating a GeoJSON layer with the retrieved data
+L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      }, 
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }  
+}).addTo(map);
+}); 
+      
